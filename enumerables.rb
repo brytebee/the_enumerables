4b45bab2f866edcfd3fbec8742f01fe891ac1270
brytebee <brytebee@gmail.com>
2021-02-26 10:44:25 +0100

    add my_each method

--- enumerables.rb
+++ enumerables.rb
@@ -5,7 +5,16 @@
 # Description/Explanation of the Enumerable module
 # containly mainly used methods.
 module Enumerable
-
+   # my_each
+   def my_each
+    return to_enum(:my_each) unless block_given?
+    i = 0
+    while i < to_a.size
+      yield(to_a[i])
+      i += 1
+    end
+    self
+  end
 end
 
 # rubocop: enable all metrics
