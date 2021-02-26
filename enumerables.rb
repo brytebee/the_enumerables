1100342db96fdc8a9c8b83cf32b52a8e5727324d
brytebee <brytebee@gmail.com>
2021-02-26 10:43:01 +0100

    add the module definition

---
+++ enumerables.rb
@@ -0,0 +1,11 @@
+# rubocop: disable all metrics
+
+# frozen_string_literal: true
+
+# Description/Explanation of the Enumerable module
+# containly mainly used methods.
+module Enumerable
+
+end
+
+# rubocop: enable all metrics
