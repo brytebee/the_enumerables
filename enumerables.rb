cf59888b2806692c69ecfa9112bdc451c17d0f60
brytebee <brytebee@gmail.com>
2021-02-26 10:50:18 +0100

    add my_all? method

--- enumerables.rb
+++ enumerables.rb
@@ -26,7 +26,7 @@ module Enumerable
       index += 1
     end
   end
-  
+
   # my_select
   def my_select
     return to_enum(:my_select) unless block_given?
@@ -36,6 +36,28 @@ module Enumerable
       array << i if yield(i)
     end
     array
+  end
+
+  # ay_all?
+  def my_all?(param = nil)
+    first_counter = 0
+    second_counter = 0
+    if block_given?
+      my_each { |i| first_counter += 1 if yield(i) }
+      first_counter == size
+    else
+      case param
+      when nil
+        my_each { |i| return true unless i.nil? || !i }
+      when Regexp
+        my_each { |i| return true if i =~ param }
+      when Class
+        my_each { |i| return true if i.is_a? param }
+      else
+        my_each { |elem| second_counter += 1 if !elem.nil? == true }
+        second_counter == size
+      end
+    end
   end
 end
 
